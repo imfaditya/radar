bf2e6d2a97990d597e0f149d90af301a1ef0f738
Imam Faraz Aditya <imamaditya2211@gmail.com>
2022-08-24 16:33:52 +0700

    fix logo di dark mode

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,8 @@
+let state = false;
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const button = document.getElementsByClassName("button-mode");
+const logo = document.querySelector("#company-logo");
 
 for(let i = 0; i < button.length; i++){
   button[i].addEventListener('click', function(event){
@@ -11,4 +13,12 @@ for(let i = 0; i < button.length; i++){
 function checkDarkMode(){
   header.classList.toggle("dark-on");
   main.classList.toggle("dark-on");
+  if(state === false){
+    state = true;
+    logo.setAttribute("src", "assets/images/logo-white.svg");
+  } else{
+    state = false;
+    logo.setAttribute("src", "assets/images/logo-color.svg");
+  }
+  
 }
